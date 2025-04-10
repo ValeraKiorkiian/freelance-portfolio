9fde1d9e6d0bbfd15305c2aba9e07a1ee5ca9120
Valerii Dzhurenko-Kiorkiian <valerakiorkian@gmail.com>
2025-04-09 21:15:31 -0400

    header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,29 +1,12 @@
-// const openMenu = document.querySelector('.js-nav-list-open');
-// const navList = document.querySelector('.nav-list');
-// const headerSection = document.querySelector('.header');
-// openMenu.addEventListener(`click`, event => {
-//   event.stopPropagation();
-//   navList.classList.toggle('is-open');
-// });
-// headerSection.addEventListener(`click`, () => {
-//   if (navList.classList.contains('is-open')) {
-//     navList.classList.toggle('is-open');
-//   }
-// });
 const openMenu = document.querySelector('.js-nav-list-open');
 const navList = document.querySelector('.nav-list');
-
-openMenu.addEventListener('click', event => {
+const headerSection = document.querySelector('.header');
+openMenu.addEventListener(`click`, event => {
   event.stopPropagation();
   navList.classList.toggle('is-open');
 });
-
-// Закрытие меню при клике вне кнопки и вне самого меню
-document.addEventListener('click', event => {
-  const isClickInsideMenu = navList.contains(event.target);
-  const isClickOnButton = openMenu.contains(event.target);
-
-  if (!isClickInsideMenu && !isClickOnButton) {
-    navList.classList.remove('is-open');
+headerSection.addEventListener(`click`, () => {
+  if (navList.classList.contains('is-open')) {
+    navList.classList.toggle('is-open');
   }
 });
