4ad1e434c2ab1cead08bf704bb4b646b04f8c873
Mykhajlo-Berezhnyj <bmve@ukr.net>
2025-04-10 07:05:29 +0300

    add fix

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -15,24 +15,66 @@ import 'swiper/css/effect-coverflow';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Accordion('#accordion-about', {
-    duration: 300,
+    duration: 900,
     showMultiple: false,
     openOnInit: [0],
   });
+});
 
+document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('#swiper-about', {
-    modules: [Navigation, Autoplay, Keyboard, EffectCreative],
+    modules: [Navigation, Autoplay, Keyboard, EffectCoverflow, EffectCreative],
     // Default parameters
     slidesPerView: 2,
     spaceBetween: 0,
     slideToClickedSlide: true,
+    effect: 'creative',
     // Responsive breakpoints
     breakpoints: {
+      320: {
+        slidesPerView: 2,
+        effect: 'creative',
+        creativeEffect: {
+          limitProgress: 2,
+          prev: {
+            translate: ['-100%', 0, 0],
+            rotate: [0, 0, -360],
+          },
+          next: {
+            translate: ['100%', 0, 0],
+            rotate: [0, 0, 360],
+          },
+        },
+      },
       768: {
         slidesPerView: 3,
+        effect: 'creative',
+        creativeEffect: {
+          limitProgress: 3,
+          prev: {
+            translate: ['-100%', 0, 0],
+            rotate: [0, 0, -360],
+          },
+          next: {
+            translate: ['100%', 0, 0],
+            rotate: [0, 0, 360],
+          },
+        },
       },
       1440: {
         slidesPerView: 6,
+        effect: 'creative',
+        creativeEffect: {
+          limitProgress: 6,
+          prev: {
+            translate: ['-100%', 0, 0],
+            rotate: [0, 0, -360],
+          },
+          next: {
+            translate: ['100%', 0, 0],
+            rotate: [0, 0, 360],
+          },
+        },
       },
     },
     loop: true,
@@ -43,18 +85,6 @@ document.addEventListener('DOMContentLoaded', () => {
       enabled: true,
       onlyInViewport: true,
       pageUpDown: true,
-    },
-    effect: 'creative',
-    creativeEffect: {
-      limitProgress: 6,
-      prev: {
-        translate: ['-100%', 0, 0],
-        rotate: [0, 0, -360],
-      },
-      next: {
-        translate: ['100%', 0, 0],
-        rotate: [0, 0, 360],
-      },
     },
     speed: 1400,
     autoplay: {
